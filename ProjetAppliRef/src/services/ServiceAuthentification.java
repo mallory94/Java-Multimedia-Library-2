423ada237a92389734ea474cf18884a058f2def5
mallory94 <malloga@hotmail.fr>
2020-02-26 01:26:01 +0100

    ajout d'une ébauche de service d'authentification nécessaire pour gérer les connexions entrantes des programmeurs

---
+++ ProjetAppliRef/src/services/ServiceAuthentification.java
@@ -0,0 +1,61 @@
+package services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServiceAuthentification extends Service implements Runnable {
+	
+	
+	public ServiceAuthentification(Socket accept) {
+		super(accept);
+	}
+
+	@Override
+	public void run() {
+		BufferedReader in = null;
+		PrintWriter out = null;
+		try {
+			in = new BufferedReader(
+					new InputStreamReader(this.getSocket().getInputStream())
+					);
+			out = new PrintWriter (this.getSocket().getOutputStream ( ), true);
+			out.println("Entrez votre identifiant");
+			String id = in.readLine();
+			if (id != null) {
+				out.println("Entrez votre mot de passe");
+				String mdp = in.readLine();
+				if (connectionValide(id,mdp)) {
+					//connecte ?
+				}
+				
+			}
+			in.close();
+			out.close();
+			
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				in.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+
+	private boolean connectionValide(String id, String mdp) {
+		
+		return false;
+	}
+	
+	
+	
+}
