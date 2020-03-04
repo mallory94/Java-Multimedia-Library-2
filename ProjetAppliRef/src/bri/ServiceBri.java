25feacfd0a15953dc92eb368ccfdf459141de530
mallory94 <malloga@hotmail.fr>
2020-03-04 11:15:46 +0100

    modif de serviceBri afin de lui permettre d'instancier et lancer les services présent dans la liste de serviceRegistry

--- ProjetAppliRef/src/bri/ServiceBri.java
+++ ProjetAppliRef/src/bri/ServiceBri.java
@@ -2,6 +2,7 @@ package bri;
 
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 import java.net.*;
 
 
@@ -16,17 +17,32 @@ class ServiceBRi implements Runnable {
 	}
 
 	public void run() {
-		try {BufferedReader in = new BufferedReader (new InputStreamReader(client.getInputStream ( )));
+		try {
+			BufferedReader in = new BufferedReader (new InputStreamReader(client.getInputStream ( )));
 			PrintWriter out = new PrintWriter (client.getOutputStream ( ), true);
 			out.println(ServiceRegistry.toStringue()+"##Tapez le numéro de service désiré : (pour l'instant vous ne pouvez qu'inverser une ligne de texte)");
 			int choix = Integer.parseInt(in.readLine());
 			// instancier le service numéro "choix" en lui passant la socket "client"
 			// invoquer run() pour cette instance ou la lancer dans un thread à part 
 			String line;
-			if (true) {
-				line = "vous avez choisi le service d'inversion de texte. Veuillez taper votre texte à inverser :";
-				out.println(line);
-				new Thread(new ServiceInversion(this.client)).start();
+			line = "vous avez choisi le service d'inversion de texte. Veuillez taper votre texte à inverser :";
+			out.println(line);
+			try {
+				new Thread(ServiceRegistry.getServiceClass(choix).getConstructor(Class.forName("Socket")).newInstance(client)).start();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
 			}
 			//rajouter un if pour ajouter un nouveau service, je précise que le service bri ne doit jamais close la socket.
 				
