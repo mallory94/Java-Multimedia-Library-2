8562ecaa88b6199f1f27bfd206f57031604366e5
mallory94 <malloga@hotmail.fr>
2020-03-15 21:36:23 +0100

    correction des erreurs de merge

--- ProjetAppliRef/src/utilisateur/ListeUtilisateur.java
+++ ProjetAppliRef/src/utilisateur/ListeUtilisateur.java
@@ -36,19 +36,19 @@ public class ListeUtilisateur {
 	}
 	
 	public static void initialisation() {
-		programmeurs.put("test",new Programmeur("test", "test", "url"));
-		programmeurs.put("mal",new Programmeur("mal", "mmm","malurl"));
+		amateurs.put("mec", new Amateur("mallory","mallory", "anonymous", "brette"));
+		amateurs.put("vad", new Amateur("vad","vvv", "vad", "vad"));
 		
 		amateurs.get("vad").AddMessage(new Message("salut mec j'aime bien les bananes au comcombres.", amateurs.get("mec")));
-		amateurs.get("vad").AddMessage(new Message("je suis pas ton p�re mec", amateurs.get("mec")));
+		amateurs.get("vad").AddMessage(new Message("je suis pas ton pire mec", amateurs.get("mec")));
 		amateurs.get("vad").AddMessage(new Message("si jamais tu est mort tu ne pourras pas me lire :'(", amateurs.get("mec")));
 		
 	}
 	
 	public static Amateur getAmateurByLogin(String login) {
+		
+		
 		return amateurs.get(login);
-		amateurs.put("mec", new Amateur("mallory","mallory", "anonymous", "brette"));
-		amateurs.put("vad", new Amateur("vad","vvv", "vad", "vad"));
 	}
 
 }
