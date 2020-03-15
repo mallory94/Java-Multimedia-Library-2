2cb365abcfd76d80bab2f8b33a7e31bc0ebf14cc
mallory94 <malloga@hotmail.fr>
2020-03-15 21:59:08 +0100

    correction du probleme survenant lors de lancement de plusieurs clients amateurs

--- ProjetAppliRef/src/bri/ServiceBRi.java
+++ ProjetAppliRef/src/bri/ServiceBRi.java
@@ -19,7 +19,13 @@ public class ServiceBRi implements Runnable {
 			BufferedReader in = new BufferedReader (new InputStreamReader(client.getInputStream ( )));
 			PrintWriter out = new PrintWriter(client.getOutputStream(),true);
 			out.println("Liste des services : ##" + ServiceRegistry.toStringue() + "##Tapez le numéro de service désiré : ");
-			int choix = Integer.parseInt(in.readLine());
+			Integer choix = null;
+			while (true) {
+				if (in.ready()) {
+					choix = Integer.parseInt(in.readLine());
+					break;
+				}
+			}
 			// instancier le service numéro "choix" en lui passant la socket "client"
 			// invoquer run() pour cette instance ou la lancer dans un thread à part
 			try {
