6b4dc3e1e2f299a8362f9d980ff227bb6f333d56
mallory94 <malloga@hotmail.fr>
2020-03-15 22:27:07 +0100

    correction de l'impossibilité d'avoir plusieurs programmeurs en même temps

--- ProjetAppliRef/src/bri/ServiceBRIprog.java
+++ ProjetAppliRef/src/bri/ServiceBRIprog.java
@@ -25,34 +25,33 @@ private Socket client;
 			PrintWriter out = new PrintWriter (client.getOutputStream ( ), true);
 			
 			String line = "";
-			while(!in.ready()) {
-				out.println("Que souhaitez vous faire ?##1 : Fournir un nouveau service##"
-						+ "2 : Mettre-à-jour un service##"
-						+ "3 : Déclarer un changement d'adresse de votre serveur ftp##"
-						+ "4 : Quitter l'application##"
-						+ "Saisissez le numéro de l'option voulue");
-				Integer choix = null;
-				try {
-					choix = Integer.parseInt(in.readLine());
-					switch (choix) {
-					case 1:
-						new ServiceUploadService(client).start();
-						break;
-					case 2:
-						break;
-					case 3:
-						break;
-					case 4:
-						break;
-					}
+			System.out.println("ok");
+			out.println("Que souhaitez vous faire ?##1 : Fournir un nouveau service##"
+					+ "2 : Mettre-à-jour un service##"
+					+ "3 : Déclarer un changement d'adresse de votre serveur ftp##"
+					+ "4 : Quitter l'application##"
+					+ "Saisissez le numéro de l'option voulue");
+			Integer choix = null;
+			try {
+				choix = Integer.parseInt(in.readLine());
+				switch (choix) {
+				case 1:
+					new ServiceUploadService(client).start();
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
 				}
-				catch (NumberFormatException e1) {
-					out.println("Erreur de saisie.## Veuillez saisir un chiffre parmi ceux proposés ");
-				}
-				
-				
-				System.out.println(line);
 			}
+			catch (NumberFormatException e1) {
+				out.println("Erreur de saisie.## Veuillez saisir un chiffre parmi ceux proposés ");
+			}
+			
+			
+			System.out.println(line);
 
 		}
 		catch (IOException e) {
